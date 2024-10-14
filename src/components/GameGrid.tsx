d99de3aa3a5f13a013232987a3446a87efeace6b
Mahshid Bahira <182437623+mahshidbahira@users.noreply.github.com>
2024-10-14 19:48:25 +0330

    implement useQuery for games

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -19,7 +19,7 @@ const GameGrid = (props: Props) => {
 
   return (
     <>
-      {error && <Text>{error}</Text>}
+      {error && <Text>{error.message}</Text>}
       <SimpleGrid
         columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
         padding="10px"
@@ -31,7 +31,7 @@ const GameGrid = (props: Props) => {
               <GameCardSkeleton />
             </GameCardContainer>
           ))}
-        {data.map((game) => (
+        {data?.results.map((game) => (
           <GameCardContainer key={game.id}>
             <GameCard game={game} />
           </GameCardContainer>
